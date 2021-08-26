fb89cbbc9d3b3f5b7e44fcf56a84dfb561045008
DESKTOP-J6BCV4Q\LC04 <mohsing523@gmail.com>
2021-08-26 08:49:21 -0700

    all done

--- resources/views/admin/layouts/include/sidebar.blade.php
+++ resources/views/admin/layouts/include/sidebar.blade.php
@@ -153,7 +153,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="" class="nav-link">
+                            <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Report's</p>
                             </a>
