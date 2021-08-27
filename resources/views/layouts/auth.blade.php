7e159dcd8066a9ae59e2caa2a49b0cac9feb3b03
Talha Akbar <talhaakbarakbar@gmail.com>
2021-08-27 05:42:33 -0700

    changes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -10,20 +10,20 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Milk Kitchen</title>
-        <link rel="stylesheet" href="{{asset('customer/css/bootstrap.css')}}" />
-        <link rel="stylesheet" href="{{asset('customer/css/bootstrap.min.css')}}" />
-        <link rel="stylesheet" href="{{asset('customer/css/style.css')}}" />
-        <link rel="stylesheet" href="{{asset('customer/css/font-awesome.min.css')}}" />
+        <link rel="stylesheet" href="{{asset('customer-panel/css/bootstrap.css')}}" />
+        <link rel="stylesheet" href="{{asset('customer-panel/css/bootstrap.min.css')}}" />
+        <link rel="stylesheet" href="{{asset('customer-panel/css/style.css')}}" />
+        <link rel="stylesheet" href="{{asset('customer-panel/css/font-awesome.min.css')}}" />
     </head>
     <body>
         @include('include.auth-header')
             @yield('content')
         @include('include.auth-footer')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="{{asset('customer/js/bootstrap.js')}}"></script>
-        <script src="{{asset('customer/js/bootstrap.min.js')}}"></script>
-        <script src="{{asset('customer/js/index.js')}}"></script>
-        <script src="{{asset('customer/js/fontawesome.js')}}"></script>
+        <script src="{{asset('customer-panel/js/bootstrap.js')}}"></script>
+        <script src="{{asset('customer-panel/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('customer-panel/js/index.js')}}"></script>
+        <script src="{{asset('customer-panel/js/fontawesome.js')}}"></script>
         @yield('scripts')
     </body>
 </html>
