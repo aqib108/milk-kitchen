cb22496d9e8f6a4de95eb38da35595ea8a5183c6
punjtan <ghulammadina2016@gmail.com>
2021-10-07 06:09:58 -0700

    runpicklist

--- resources/views/admin/customer/masterPicklist.blade.php
+++ resources/views/admin/customer/masterPicklist.blade.php
@@ -23,95 +23,90 @@
     </div><!-- /.container-fluid -->
 </section>
 <div class="col-md-12 col-sm-12 col-xs-12">
-                    <div class="x_panel">
-                        <div class="x_title">
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="x_content">
-                            <!-- 'url'=>route('billing') -->
-                            <!-- {{ Form::model(array('method' => 'post','id'=>"search-delivery", 'class'=>"form-horizontal form-label-left")) }} -->
-                            <div class="row">
-                            <div class="form-group col-sm-4">
-                                <select name='warehouse' class="form-control" id="warehouse" placeholder='select Warehouse'>
-                                   <option selected disabled>Slect WareHouse</option> 
-                                    @foreach($warehouses as $warehouse)
-                                    @php $selected = ''; @endphp
-                                        @if($loop->first)
-                                        @php $selected = 'selected="selected"'; @endphp
-                                        @endif
-                                    <option value='{{$warehouse->id}}' {{$selected}}>{{$warehouse->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                       
-                            <div class="form-group col-sm-4">
-                                <select name='day' class="form-control" id="day_id" placeholder='select Day'>
-                                   <option selected disabled>Slect Day</option> 
-                                    @foreach($days as $day)
-                                    <option value='{{$day->id}}'>{{$day->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            </div>
+    <div class="x_panel">
+        <div class="x_title">
+            <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+            <!-- 'url'=>route('billing') -->
+            <!-- {{ Form::model(array('method' => 'post','id'=>"search-delivery", 'class'=>"form-horizontal form-label-left")) }} -->
+            <div class="row">
+                <div class="form-group col-sm-4">
+                    <select name='warehouse' class="form-control" id="warehouse" placeholder='select Warehouse'>
+                        <option selected disabled>Slect WareHouse</option>
+                        @foreach($warehouses as $warehouse)
+                        @php $selected = ''; @endphp
+                        @if($loop->first)
+                        @php $selected = 'selected="selected"'; @endphp
+                        @endif
+                        <option value='{{$warehouse->id}}' {{$selected}}>{{$warehouse->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
 
-<!-- 
+                <div class="form-group col-sm-4">
+                    <select name='day' class="form-control" id="day_id" placeholder='select Day'>
+                        <option selected disabled>Slect Day</option>
+                        @foreach($days as $day)
+                        <option value='{{$day->id}}'>{{$day->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <!-- 
                             <div class="form-group col-sm-3">
                                 <br />
                                 <button class="btn btn-success search-btn" id="submit_btn" value="search" >Search Deliveries</button>
                             </div> -->
-                            <!-- {{ Form::close() }} -->
-                        </div>
-                    </div>
-                </div>
+            <!-- {{ Form::close() }} -->
+        </div>
+    </div>
+</div>
 <!-- Main content -->
 
 <section class="content">
-        <div class="container-fluid">
-            <div>
+    <div class="container-fluid">
+        <div>
             <p id="deliverRecord">
-            </p> 
+            </p>
             <br>
         </div>
         <!-- /.container-fluid -->
-    </section>
+</section>
 <!-- /.content -->
 @endsection
 
 @section('scripts')
 <script>
-       window.onload =function() {
-        $.ajax({
-            method: "post",
-            url: "{{route('getmasterPicklist')}}",
-            data: {
-                _token: $('meta[name="csrf_token"]').attr('content'),
-            },
-            success: function(response) {
-                $('#deliverRecord').empty();
-                $('#deliverRecord').append(response.html);
-            }
-        });        
-       };
-    $(document).ready(function() {
-    $('#warehouse,#day_id').on('change', function() {
-        var warehouse_id = $('#warehouse').val();
-        var day_id = $('#day_id').val();
-        var option = '';
+    window.onload = function() {
+        masterpicklist();
+    };
 
+    function masterpicklist(warehouseId = null, dayId = null) {
         $.ajax({
             method: "post",
             url: "{{route('getmasterPicklist')}}",
             data: {
                 _token: $('meta[name="csrf_token"]').attr('content'),
-                id: warehouse_id,
-                day_id:day_id,
+                id: warehouseId,
+                day_id: dayId,
             },
             success: function(response) {
                 $('#deliverRecord').empty();
                 $('#deliverRecord').append(response.html);
             }
         });
-    });});
+    }
+    $(document).ready(function() {
+        $('#warehouse,#day_id').on('change', function() {
+            var warehouseId = $('#warehouse').val();
+        
+            var dayId = $('#day_id').val();
+            masterpicklist(warehouseId,dayId)
+
+        });
+    });
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="{{ asset('customer-panel/js/index.js') }}"></script>
