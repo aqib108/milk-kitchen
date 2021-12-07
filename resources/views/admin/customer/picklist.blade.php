483bbe6af50e7986152e23643113b93f2c481194
punjtan <ghulammadina2016@gmail.com>
2021-12-07 20:12:01 +0500

    ar code

--- resources/views/admin/customer/picklist.blade.php
+++ resources/views/admin/customer/picklist.blade.php
@@ -179,32 +179,16 @@
                             </thead>
                             <tbody class="week-container-tbl">
                                 @foreach ($products as $product)
-                                    <tr class="week_days_1" data-p-id="{{ $product->id }}">
-                                        @foreach ($weekDays as $item)
-                                            @if ($item->productOrder->isNotEmpty())
-                                                @if ($item->id == $orderDetail->day_id)
-                                                    @foreach ($item->productOrder as $order)
-                                                        @if ($order->quantity == 0)
-                                                            @continue;
-                                                        @endif
-                                                        @if($order->user_id == $orderDetail->user_id)
-                                                            @if ($order->product_id == $product->id)
-                                                                <td class="table-td-wrapper" scope="row" style="background-color: white !important; width: 175px; text-align:center;">
-                                                                    {{ $product->name }}
-                                                                </td>
-                                                                <td style="background-color: white !important; width: 175px; text-align:center;">
-                                                                    <div class="quantity" >
-                                                                        {{ $order->quantity }}
-                                                                    </div>
-                                                                    <input type="hidden" name="totalCtn[]"
-                                                                        value="{{ $order->quantity }}">
-                                                                </td>
-                                                            @endif
-                                                        @endif
-                                                    @endforeach
-                                                @endif
-                                            @endif
-                                        @endforeach
+                                    <tr class="week_days_1">
+                                    <td class="table-td-wrapper" scope="row" style="background-color: white !important; width: 175px; text-align:center;">
+                                        {{ $product['name'] }}
+                                    </td>
+                                    <td style="background-color: white !important; width: 175px; text-align:center;">
+                                        <div class="quantity" >
+                                            {{ $product['carton'] }}
+                                        </div>
+
+                                    </td>
                                     </tr>
                                 @endforeach
                             </tbody>
@@ -228,7 +212,7 @@
                 </div>
                 @if(auth()->user()->hasRole('Driver') || auth()->user()->hasRole('Admin'))
                     <div class="col-lg-4">
-                        {!! QrCode::size(150)->generate(route('qr.driverScan',['id'=>$customerID,'productId'=>$productOrderId])) !!}
+                        {!! QrCode::size(150)->generate(route('qr.driverScan',['id'=>$customer[0]->id,'productId'=>$productOrderId])) !!}
                     </div>
                 @else
                     <div class="col-lg-4">
@@ -241,81 +225,7 @@
                     </div>
                 @endif 
             </div>
-            <section>
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="heading-tbl">Edit Delivery</h2>
-                    </div>
-                    <div class="col-md-12">
-                        <div>
-                            <table class="table table-bordered mb-0 weekly_standing_order tbl-left-box">
-                                <thead>
-                                    <tr>
-                                        <th class="table-th-wrapper" scope="col">Product</th>
-                                        <th class="table-th-wrapper" scope="col">
-                                            @foreach ($weekDays as $item)
-                                                @if ($item->productOrder->isNotEmpty())
-                                                    @if ($item->id == $orderDetail->day_id)
-                                                        {{ $item->name }}
-                                                    @endif
-                                                @endif
-                                            @endforeach
-                                        </th>
-                                        <th class="table-th-wrapper" scope="col">Actually received
-                                        </th>
-
-
-                                    </tr>
-
-                                </thead>
-
-                                <tbody class="week-container-tbl" style="text-align: center;">
-                                    @php
-                                        $count = 0;
-                                        $order_id = 0;
-                                    @endphp
-
-                                    @foreach ($products as $product)
-                                        <tr class="week_days_1" data-p-id="{{ $product->id }}">
-                                        <td class="table-td-wrapper" scope="row" style="background-color: white !important;">
-                                                                        {{ $product->name }}</td>
-                                                                    <td style="background-color: white !important;">
-                                                                        <div class="quantity">
-                                                                            {{ $order->quantity }}
-                                                                        </div>
-                                                                        <input type="hidden" class="t_ctn"
-                                                                            value="{{ $order->quantity }}">
-                                                                    </td>
-                                                                    <td style="background-color: white !important;">
-                                                                        <input type="number" class="d_qnty" data-id="{{$orderDetail->day_id}}" style="width: 80px;
-                                                                        text-align: center;" minlength="0"
-                                                                            value="{{ !empty($order->d_qnty)?$order->d_qnty:0 }}" disabled>
-                                                                    </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            <table class="table table-bordered mb-0 tbl-right-box">
-                                <thead>
-                                    <tr>
-                                        <th class="table-th-wrapper" scope="col">Reason</th>
-                                    </tr>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <div class="form-group">
-                                                <textarea class="form-control" id="exampleFormControlTextarea1"
-                                                    rows="3"></textarea>
-                                            </div>
-                                        </td>
-                                    </tr>
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </section>
+           
             <div class="col-lg-12">
                 <h2 class="heading-tbl">Edit History</h2>
             </div>
@@ -334,76 +244,4 @@
         <!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-@endsection
-
-@section('scripts')
-
-    <script>
-        $(document).ready(function() {
-            var total = 0;
-            $('input[name="totalCtn[]"]').each(function() {
-                if ($(this).val() !== '') {
-                    total += parseInt($(this).val());
-                }
-            });
-            total_ctns = total;
-            $('#total_ctns').append(total_ctns);
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-            $('body').on('change', '.weekly_standing_order .week_days_1 td input', function() {
-                let product_id = $(this).parent('td').parent('tr').attr('data-p-id');
-                console.log(product_id);
-                let day_id = $(this).attr('data-id');
-                console.log(day_id)
-                let qnty = $(this).val();
-                console.log(qnty)
-                if (qnty < 0) {
-                    Swal.fire({
-                        position: 'top-end',
-                        toast: true,
-                        showConfirmButton: false,
-                        timer: 2000,
-                        icon: 'error',
-                        title: 'Quantity should not be less than 0',
-                    });
-                    $(this).val(0);
-                } else {
-                    $.ajax({
-                        type: "POST",
-                        data: {
-                            'day_id': day_id,
-                            'product_id': product_id,
-                            'qnty': qnty
-                        },
-                        url: "{{ route('customer.edit-delivery-orders', $customerID) }}",
-                        success: function(response) {
-                            if (response.status == true) {
-                                Swal.fire({
-                                    position: 'top-end',
-                                    toast: true,
-                                    showConfirmButton: false,
-                                    timer: 2000,
-                                    icon: 'success',
-                                    title: response.message,
-                                });
-                            } else {
-                                Swal.fire({
-                                    position: 'top-end',
-                                    toast: true,
-                                    showConfirmButton: false,
-                                    timer: 2000,
-                                    icon: 'error',
-                                    title: response.success,
-                                });
-                            }
-                        },
-                    });
-
-                }
-            });
-        });
-    </script>
 @endsection
